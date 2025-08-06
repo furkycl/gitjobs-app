10d876e3513e8f21692c99575995939f8ff46c35
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-08-06 23:18:14 +0300

    feat(setup): Complete initial project setup with Vite, React, Redux & Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    // Arka planı koyu gri, yazıyı beyaz ve büyük yapalım
+    <div className="bg-slate-800 min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-center text-white p-8 border-4 border-teal-400 rounded-lg">
+        GitJobs - Tailwind Kurulumu Başarılı!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
